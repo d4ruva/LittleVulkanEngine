a8de5b122c1eb8efaf4693bbfcfc0ac7c14dfe54
d4ruva <dhruvadhangar@gmail.com>
2024-04-28 12:34:23 +0530

    Added Vulkan Device Setup Code Part 2

--- LittleVulkanEngine/src/lve_pipeline.hpp
+++ LittleVulkanEngine/src/lve_pipeline.hpp
@@ -7,14 +7,28 @@
 
 namespace lve
 {
-	struct PipelineConfigInfo {};
+	struct PipelineConfigInfo 
+	{
+		VkViewport viewport;
+		VkRect2D scissor;
+		VkPipelineViewportStateCreateInfo viewportInfo;
+		VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+		VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+		VkPipelineMultisampleStateCreateInfo multisampleInfo;
+		VkPipelineColorBlendAttachmentState colorBlendAttachment;
+		VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+		VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+		VkPipelineLayout pipelineLayout = nullptr;
+		VkRenderPass renderPass = nullptr;
+		uint32_t subpass = 0;
+	};
 
 	class LvePipeline
 	{
 	public:
 		LvePipeline(LveDevice &device, const std::string& vertFilePath, const std::string& fragFilePath, const PipelineConfigInfo& configInfo);
 
-		~LvePipeline() {};
+		~LvePipeline();
 		
 		static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
 
